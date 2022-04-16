9f13b1113fca407ce531aa925f0546d06d571a0d
khaled22 <khaled@sherzal.com>
2022-04-16 03:51:55 +0200

    exam component init

---
+++ src/components/Exam/Exam.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Exam = () => {
+   return (
+      <div className='center'>
+         <div className='form pa4 br3 shadow-5 w-70 center'>
+            <p className=''>
+               Question ??
+            </p>
+            <input className='center f4 pa2 w-70' type='text' />
+         </div>
+
+      </div>
+   );
+}
+
+export default Exam;
